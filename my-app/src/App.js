200b9e42848dd56e22089e5648a938fab7aeb23f
Samhitha Tarra <samhitht@uci.edu>
2023-04-13 12:49:44 -0400

    update with colors

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './App.css';
 
 function App() {
   const [file1, setFile1] = useState(null);
@@ -25,16 +26,61 @@ function App() {
       .then((data) => console.log(data))
       .catch((error) => console.error(error));
   };
+  const handleDragOver = (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+  };
+
+  const handleDrop = (event, fileHandler) => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    const file = event.dataTransfer.files[0];
+
+    fileHandler(file);
+  };
+
+
+
 
   return (
-    <div>
-      <h1>Upload Files</h1>
-      <input type="file" accept="text/csv" onChange={handleFile1Upload} />
-      <br />
-      <input type="file" accept="text/csv" onChange={handleFile2Upload} />
-      <br />
-      <button onClick={handleUpload}>Upload</button>
+    
+    <body>
+  <div className="container">
+    <div className="square square-1" onDragOver={handleDragOver}
+          onDrop={(event) => handleDrop(event, setFile1)}>
+      <div className="file-upload">
+        <label htmlFor="file1" className="file-upload-label">
+          {file1 ? file1.name : "Upload Voters File"}
+        </label>
+        <input type="file" id="file1" accept="text/csv" onChange={handleFile1Upload} />
+      </div>
     </div>
+    <div className="square square-2" onDragOver={handleDragOver}
+          onDrop={(event) => handleDrop(event, setFile2)}>
+      <div className="file-upload">
+        <label htmlFor="file2" className="file-upload-label">
+          {file2 ? file2.name : "Upload Votes File"}
+        </label>
+        <input type="file" id="file2" accept="text/csv" onChange={handleFile2Upload} />
+      </div>
+    </div>
+    <br />
+    <button onClick={handleUpload}>Upload</button>
+  </div>
+  <div class="upload-status">
+      <p>Upload status goes here</p>
+    </div>
+
+
+
+</body>
+
+
+
+
+
+
   );
 }
 
