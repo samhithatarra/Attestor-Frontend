b7fca95868f7e9f44c9c07afcb1f47b495913de8
Samhitha Tarra <samhitht@uci.edu>
2023-04-15 21:01:15 -0400

    tailwind css added and formatting

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,19 @@
-import React, { useState, useRef } from 'react';
+import React, { useRef } from 'react';
 import './App.css';
 
 function App() {
   const jsonTextarea1Ref = useRef(null);
   const jsonTextarea2Ref = useRef(null);
 
+
   const handleUpload = () => {
     const inputCount = document.getElementById('inputCount').value;
     const inputMessage = document.getElementById('inputMessage').value;
 
+
     fetch('http://localhost:5000/upload', {
       method: 'POST',
-      body: JSON.stringify({ inputCount: inputCount, inputMessage: inputMessage}),
+      body: JSON.stringify({ inputCount: inputCount, inputMessage: inputMessage }),
       headers: {
         'Content-Type': 'application/json'
       }
@@ -19,7 +21,7 @@ function App() {
       .then((response) => response.json())
       .then((data) => {
         jsonTextarea1Ref.current.value = JSON.stringify(data.data1, null, 2);
-        jsonTextarea2Ref.current.value = JSON.stringify(data, null, 2);
+        jsonTextarea2Ref.current.value = JSON.stringify(data.data2, null, 2);
       })
       .catch((error) => console.error(error));
   };
@@ -31,17 +33,29 @@ function App() {
       </div>
 
       <div className="square">
-        <input id='inputCount' placeholder='Number of Validators' type="text" />
-        <input id='inputMessage' placeholder='Message to be Proved' type="text" />
-
-        <button onClick={handleUpload}>Upload</button>
+        <input
+          id="inputCount"
+          type="text"
+          placeholder="Number of Validators"
+          class="w-80 appearance-none rounded-full border-0 bg-slate-800/50 p-2 px-4 focus:bg-slate-800 focus:ring-2 focus:ring-orange-500"
+        />
+        <input
+          id="inputMessage"
+          type="text"
+          placeholder="Message to be Proved"
+          class="w-80 appearance-none rounded-full border-0 bg-slate-800/50 p-2 px-4 focus:bg-slate-800 focus:ring-2 focus:ring-orange-500"
+        />
+        <button class="rounded-full border-0" onClick={handleUpload}>Generate</button>
       </div>
+
       <div className="upload-status">
         <div className="scrollable">
+          <label htmlFor="json-textarea-1">Certificate</label>
           <textarea id="json-textarea-1" readonly="yes" ref={jsonTextarea1Ref} wrap="soft" rows="10"></textarea>
         </div>
         <div className="spacer"></div>
         <div className="scrollable">
+          <label htmlFor="json-textarea-2">Proof</label>
           <textarea id="json-textarea-2" readonly="yes" ref={jsonTextarea2Ref} wrap="soft" rows="10"></textarea>
         </div>
       </div>
